4c255dade0c31994b858e5e9009c132a2079b68f
pemiinem <190139323@aluno.unb.br>
2025-09-22 17:16:42 -0300

    style: change buttons from written to icons

--- companies-app/src/components/CompanyList.vue
+++ companies-app/src/components/CompanyList.vue
@@ -10,7 +10,7 @@
           type="text"
         />
         <button @click="onSearch" class="btn-search">
-          <img src="./assets/search-icon.svg" alt="Buscar" />
+          <img src="../assets/icons/search-icon.svg" alt="Buscar" />
         </button>
       </div>
       <router-link to="/companies/new" class="btn-new">Adicionar Empresa</router-link>
@@ -31,8 +31,12 @@
           <p>CNPJ: {{ c.cnpj }} - Email: {{ c.email }}</p>
         </div>
         <div class="company-actions">
-          <router-link :to="`/companies/${c.id}/edit`" class="btn-edit">Editar</router-link>
-          <button @click="$emit('delete', c.id)" class="btn-delete">Excluir</button>
+          <router-link :to="`/companies/${c.id}/edit`" class="action-icon" title="Editar">
+            <img src="../assets/icons/edit.svg" alt="Editar" />
+          </router-link>
+          <button @click="$emit('delete', c.id)" class="action-icon" title="Excluir">
+            <img src="../assets/icons/trash.svg" alt="Excluir" />
+          </button>
         </div>
       </div>
 
@@ -61,3 +65,39 @@ function onSearch() {
   emit('search', search.value)
 }
 </script>
+
+<style scoped>
+.company-actions {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  justify-content: flex-end;
+}
+
+.action-icon {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  transition: background-color 0.2s ease-in-out;
+  text-decoration: none;
+}
+
+.action-icon:hover {
+  background-color: #e9ecef;
+}
+
+.action-icon img {
+  width: 16px;
+  height: 16px;
+}
+
+button.action-icon {
+  padding: 0;
+}
+</style>
