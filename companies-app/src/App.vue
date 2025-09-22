810e160e3e32ea865749699bb7dcff7ae5a75412
pemiinem <190139323@aluno.unb.br>
2025-09-22 15:40:10 -0300

    fix: correct logo filename in App.vue to ensure proper asset loading

--- companies-app/src/App.vue
+++ companies-app/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <nav class="navbar">
-      <img src="./assets/opea_logo.svg" alt="Opea Logo" style="height:32px; margin-right:16px;" />
+      <img src="./assets/opea-logo.svg" alt="Opea Logo" style="height:32px; margin-right:16px;" />
       <a href="/">Home</a>
       <router-link to="/companies">Empresas</router-link>
     </nav>
