9878a88ec1a80a8555f5057f2efbadc530edb0e6
pemiinem <190139323@aluno.unb.br>
2025-09-24 15:10:25 -0300

    deploy: fix vite file

--- companies-app/vite.config.js
+++ companies-app/vite.config.js
@@ -4,4 +4,13 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: '/opea-challenge/',
+  build: {
+    rollupOptions: {},
+    commonjsOptions: {
+      include: [/node_modules/]
+    }
+  },
+  optimizeDeps: {
+    include: ['vue-toastification']
+  }
 })
